8e1d970125e8a43bcfd26e484b1356899cb82d73
ania <AnSandhuCh@smail.pembinatrails.ca>
2024-10-02 14:04:31 -0500

    Resolve merge conflict between master and add-monster

--- game.sh
+++ game.sh
@@ -1,5 +1,5 @@
 subl game.sh
-echo "Welcome to the Ultimate Adventure Game!"
+echo "Welcome to the Ultimate Adventure Game with Monsters!"
 echo "Choose a location to explore: forest, castle, or cave."
 read location
 if [ "$location" == "forest" ]; then
