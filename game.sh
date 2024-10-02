a14bc26f44e325b82688cd71a37021c1cd98e690
ania <AnSandhuCh@smail.pembinatrails.ca>
2024-10-02 13:41:36 -0500

    Add monster feature to the game

--- game.sh
+++ game.sh
@@ -2,11 +2,11 @@ subl game.sh
 echo "Welcome to the Ultimate Adventure Game!"
 echo "Choose a location to explore: forest, castle, or cave."
 read location
-if [ "Slocation" -- "forest" ]; then
+if [ "$location" == "forest" ]; then
 cat forest.txt
-elif [ "Slocation" -- "castle"]; then
+elif [ "$location" == "castle"]; then
 cat castle.txt
-elif [ "Slocation" -- "cave" ]; then
+elif [ "$location" == "cave" ]; then
 cat cave.txt
 echo "But wait..."
 cat monster.txt
